060f4d843a776c7249b96749c6763a840dc5f7b7
Ivonne Carrillo <ivonne.carrillo@bixal.com>
2018-04-19 14:50:33 -0400

    Clean parse module code, sort of

--- web/modules/custom/parser/src/Reader/CsvReader.php
+++ web/modules/custom/parser/src/Reader/CsvReader.php
@@ -13,12 +13,12 @@ use Drupal\parser\Reader\Reader;
 class CsvReader implements Reader {
 
   public function parse($csvFile) {
-    // if (!is_file($filename)) {
-    //   throw new \RuntimeException(sprintf('File "%s" does not exist.', $csvFile));
-    // }
-    // if (!is_readable($filename)) {
-    //     throw new \RuntimeException(sprintf('File "%s" cannot be read.', $csvFile));
-    // }
+    if (!is_file($filename)) {
+      throw new \RuntimeException(sprintf('File "%s" does not exist.', $csvFile));
+    }
+    if (!is_readable($filename)) {
+        throw new \RuntimeException(sprintf('File "%s" cannot be read.', $csvFile));
+    }
     $csv = array_map('str_getcsv', file($csvFile));
     return $csv;
   }
