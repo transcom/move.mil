1c3ee9f2299592b512c4f8658f665975f4c35a8e
Ivonne Carrillo <ivonne.carrillo@bixal.com>
2018-04-23 12:45:39 -0400

    Add parser that generates zip5.json

--- web/modules/custom/parser/src/Writer/Json/Zip5Writer.php
+++ web/modules/custom/parser/src/Writer/Json/Zip5Writer.php
@@ -3,6 +3,7 @@
 namespace Drupal\parser\Writer\Json;
 
 use Drupal\parser\Writer\WriterInterface;
+use Drupal\parser\Writer\Json\JsonWriter;
 
 /**
  * Class Zip5Writer.
@@ -10,13 +11,36 @@ use Drupal\parser\Writer\WriterInterface;
  * Parse a given array and returns a JSON structure.
  */
 class Zip5Writer implements WriterInterface {
+  use JsonWriter;
 
   /**
-   * Writes zip5.json.
+   * Normalizes data then writes zip5.json.
    */
   public function write(array $rawdata) {
-    $json = "'finished':'success'";
-    return $json;
+    $zip5s = $this->mapdata($rawdata);
+    $this->writeJson($zip5s, 'zip5.json');
+  }
+
+  /**
+   * Normalizes data mapping zip5 code with the rest of the data.
+   */
+  private function mapdata(array $rawdata) {
+    $zip5s = [];
+    while ($zip5 = current($rawdata)) {
+      $zip5s[$zip5[0]] = array_combine($this->zip5headers(), $zip5);
+      next($rawdata);
+    }
+    return $zip5s;
+  }
+
+  /**
+   * Returns the zip5 headers.
+   */
+  private function zip5headers() {
+    return [
+      'zip5',
+      'service_area',
+    ];
   }
 
 }
