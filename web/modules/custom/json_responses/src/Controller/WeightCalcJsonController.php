808d2172034de7265795793265c33efe92b980d0
davey-gov <davey.oudshoorn@bixal.com>
2018-05-01 10:51:24 -0400

    refactored code back to non-functional programming and added error-handling

--- web/modules/custom/json_responses/src/Controller/WeightCalcJsonController.php
+++ web/modules/custom/json_responses/src/Controller/WeightCalcJsonController.php
@@ -19,39 +19,45 @@ class WeightCalcJsonController extends ControllerBase {
    */
   public function response() {
     $finder = new Finder();
-    $finder->files()->in(__DIR__)->name('test.json');
+    $finder->files()->in(DRUPAL_ROOT . '/../lib/data/json-data')->name('household_goods_weights.json');
     $file = NULL;
 
     foreach ($finder as $f) {
       $file = $f->getContents();
     }
 
+    if ($file == NULL) {
+      return JsonResponse::create('File not Found', 404);
+    }
+
     $file_as_string = str_replace("\n", "", $file);
     $json_obj = json_decode($file_as_string);
 
-    $array_obj = array_map(function ($obj) {
-      $items = array_map(function ($item) {
-        return [
-          preg_replace('/[^\p{L}\p{N}\s]/u', '', str_replace(' ', '-', $item->name)) => [
-            "displayName" => $item->name,
-            "weight" => $item->weight,
-          ],
-        ];
-      }, $obj->household_goods);
+    $array_obj = NULL;
 
-      return [
-        preg_replace('/[^\p{L}\p{N}\s]/u', '', str_replace(' ', '-', $obj->name)) => [
-          'displayName' => $obj->name,
-          "icon" => $obj->icon,
-          "items" => $items,
-        ],
-      ];
-    }, $json_obj);
+    foreach ($json_obj as $obj) {
+      foreach ($obj->household_goods as $item) {
+        $items[preg_replace('/[^\p{L}\p{N}\s]/u', '', str_replace(' ', '-', $item->name))] = array(
+          'displayName' => $item->name,
+          'weight' => $item->weight,
+        );
+      }
+
+      $array_obj[preg_replace('/[^\p{L}\p{N}\s]/u', '', str_replace(' ', '-', $obj->name))] = array(
+        "displayName" => $obj->name,
+        "icon" => $obj->icon,
+        "items" => $items,
+      );
+    }
 
     $response = JsonResponse::create($array_obj, 200);
-    $response->setEncodingOptions($response->getEncodingOptions() | JSON_PRETTY_PRINT);
-
-    return $response;
+    $response->setEncodingOptions($response->getEncodingOptions() | JSON_PRETTY_PRINT | JSON_FORCE_OBJECT);
+    if (gettype($response) == 'object') {
+      return $response;
+    }
+    else {
+      return JsonResponse::create('error while creating response', 500);
+    }
   }
 
 }
