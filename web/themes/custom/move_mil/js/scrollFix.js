d0dc89c88a40d839c33fb530e480a9f48a375591
Jeremy Koulish <jeremy.koulish@bixal.com>
2018-05-02 13:17:26 -0400

    fix the smooth scroll on jump links, ensure special jump link cases are handled properly, customize current page identifier on mobile

--- web/themes/custom/move_mil/js/scrollFix.js
+++ web/themes/custom/move_mil/js/scrollFix.js
@@ -1,24 +1,26 @@
-// (function($) {
-//   var offsetJumpScroll = function() {
-//     var jumpLink = $('.jump-link a');
-//     jumpLink.click(function(e) {
-//       var href = $(this).attr('href');
-//
-//       console.log(href);
-//       e.preventDefault();
-//       var targetId = href.substring(1, href.length);
-//       var target = $("div[id*='" + targetId + "']");
-//       var offset = $(target).offset();
-//       offset.top -= 200;
-//       $('html, body').animate({
-//         scrollTop: offset.top,
-//         scrollLeft: 0
-//       }, 1000);
-//       return;
-//     });
-//   };
-//
-//   $(window).on('load', function() {
-//     offsetJumpScroll();
-//   });
-// })(jQuery);
+(function($) {
+  var offsetJumpScroll = function() {
+    var jumpLink = $('.jump-link a');
+    jumpLink.click(function(e) {
+      var href = $(this).attr('href');
+      if(href[0] === '#' || (href.indexOf(window.location.pathname) > -1 && href.indexOf('#') > -1)){
+        e.preventDefault();
+        var targetId = href.split('#')[1];
+        // var targetId = href.substring(1, href.length);
+        var target = $("div[id*='" + targetId + "']");
+        var offset = $(target).offset();
+        offset.top -= 200;
+        $('html, body').animate({
+          scrollTop: offset.top,
+          scrollLeft: 0
+        }, 1000);
+        window.location.hash = targetId;
+      }
+      return;
+    });
+  };
+
+  $(window).on('load', function() {
+    offsetJumpScroll();
+  });
+})(jQuery);
