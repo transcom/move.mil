17fbb85c34b90772d343705750bb2ebd42adc8c9
Jeremy Koulish <jeremy.koulish@bixal.com>
2018-05-02 12:28:52 -0400

    pretty ids generated on blocks & paragraphs with associated jump links, and markup/theming for custom-added jump links

--- web/themes/custom/move_mil/js/scrollFix.js
+++ web/themes/custom/move_mil/js/scrollFix.js
@@ -1,22 +1,24 @@
-(function($) {
-  var offsetJumpScroll = function() {
-    var jumpLink = $('.jump-link a');
-    jumpLink.click(function(e) {
-      e.preventDefault();
-      var hrefRaw = $(this).attr('href');
-      href = hrefRaw.substring(1, hrefRaw.length);
-      var target = $("div[id*='" + href + "']");
-      var offset = $(target).offset();
-      offset.top -= 200;
-      $('html, body').animate({
-        scrollTop: offset.top,
-        scrollLeft: 0
-      }, 1000);
-      return;
-    });
-  };
-
-  $(window).on('load', function() {
-    offsetJumpScroll();
-  });
-})(jQuery);
+// (function($) {
+//   var offsetJumpScroll = function() {
+//     var jumpLink = $('.jump-link a');
+//     jumpLink.click(function(e) {
+//       var href = $(this).attr('href');
+//
+//       console.log(href);
+//       e.preventDefault();
+//       var targetId = href.substring(1, href.length);
+//       var target = $("div[id*='" + targetId + "']");
+//       var offset = $(target).offset();
+//       offset.top -= 200;
+//       $('html, body').animate({
+//         scrollTop: offset.top,
+//         scrollLeft: 0
+//       }, 1000);
+//       return;
+//     });
+//   };
+//
+//   $(window).on('load', function() {
+//     offsetJumpScroll();
+//   });
+// })(jQuery);
