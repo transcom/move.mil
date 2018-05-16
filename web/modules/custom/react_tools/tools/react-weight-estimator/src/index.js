4fa0404fc47beb429f202d59ae24fd7eec2e8da3
Justin <justin.sarnik@bixal.com>
2018-05-16 14:26:21 -0400

    remove dev depencies and add read me

--- web/modules/custom/react_tools/tools/react-weight-estimator/src/index.js
+++ web/modules/custom/react_tools/tools/react-weight-estimator/src/index.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import './css/module.css'
+import './localcss/main.css'
 
 const mountNode = document.getElementById('weight-estimator');
 
