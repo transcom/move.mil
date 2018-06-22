5dd4de60112b2675519df881a4910bd3bd164e3a
Gerardo Maldonado <gerardo.maldonado+1@gmail.com>
2018-06-22 10:55:45 -0400

    Removed service workers from react tools since we don't use/need them

--- web/modules/custom/react_tools/tools/react-weight-estimator/src/index.js
+++ web/modules/custom/react_tools/tools/react-weight-estimator/src/index.js
@@ -3,10 +3,8 @@ import Set from 'core-js/es6/set';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import './localcss/main.css'
 
 const mountNode = document.getElementById('weight-estimator');
 
 ReactDOM.render(<App />, mountNode);
-registerServiceWorker();
