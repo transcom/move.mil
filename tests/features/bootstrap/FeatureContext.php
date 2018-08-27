80473431730fcdf57bacccbec6a282f9e8adbcd3
Ivonne Carrillo <ivonne.carrillo@bixal.com>
2018-08-27 08:16:08 -0500

    Add entitlements steps

--- tests/features/bootstrap/FeatureContext.php
+++ tests/features/bootstrap/FeatureContext.php
@@ -1,6 +1,8 @@
 <?php
 
 use Drupal\DrupalExtension\Context\RawDrupalContext;
+use Behat\Mink\Exception\ExpectationException;
+use Behat\Mink\Exception\ResponseTextException;
 
 /**
  * Defines general application features used by other feature files.
@@ -9,198 +11,174 @@ use Drupal\DrupalExtension\Context\RawDrupalContext;
  */
 class FeatureContext extends RawDrupalContext {
   
-  
+  private $databaseConnection;
+
   /**
    * @Given there is an entitlement :entitlement
    */
   public function thereIsAnEntitlement($entitlement) {
-    $connection = \Drupal::service('database');
     $record = $this->entitlements()[$entitlement];
-    $connection
-      ->insert('parser_entitlements')
+    $this->insertToDb('parser_entitlements', $record);
+  }
+
+  /**
+   * @Given there is PPM data
+   */
+  public function thereIsPpmData() {
+    $zip3s = $this->zip3s();
+    foreach ($zip3s as $zip3) {
+      $this->insertToDb('parser_zip3s', $zip3);
+    }
+    $zipCodes = $this->zipCodes();
+    foreach ($zipCodes as $zipCode) {
+      $this->insertToDb('parser_zipcodes', $zipCode);
+    }
+    $serviceAreas = $this->serviceAreas();
+    foreach ($serviceAreas as $serviceArea) {
+      $this->insertToDb('parser_service_areas', $serviceArea);
+    }
+  }
+  
+  private function insertToDb($table, $record) {
+    if (empty($this->databaseConnection)) {
+      $this->databaseConnection = \Drupal::service('database');
+    }
+    $this->databaseConnection
+      ->insert($table)
       ->fields($record)
       ->execute();
   }
-  
+
+  /**
+   * Wait for a HTTP response.
+   *
+   * @When I wait :seconds seconds until I get a response with text :text
+   */
+  public function iWaitSecondsForResponse($seconds, $text) {
+    $i = 0;
+    while ($i < $seconds) {
+      try {
+        $this->assertSession()->responseContains($text);
+        return;
+      }
+      catch (ExpectationException $e) {
+        ++$i;
+        sleep(1);
+      }
+    }
+    $message = "The text '$text' was not found after a $seconds seconds timeout";
+    throw new ResponseTextException($message, $this->getSession());
+  }
+
+  /**
+   * Wait for a element.
+   *
+   * @When I wait :seconds seconds until I see text :text
+   */
+  public function iWaitSecondsForElement($seconds, $text) {
+    $i = 0;
+    while ($i < $seconds) {
+      try {
+        $this->assertSession()->pageTextContains($text);
+        return;
+      }
+      catch (ResponseTextException $e) {
+        ++$i;
+        sleep(1);
+      }
+    }
+    $message = "The text '$text' was not found after a $seconds seconds timeout";
+    throw new ResponseTextException($message, $this->getSession());
+  }
+
   private function entitlements() {
     return [
-      'service-academy-cadet-midshipma' => [
-        'rank'=> 'Service Academy Cadet/Midshipman',
-        'total_weight_self'=> '350',
-        'total_weight_self_plus_dependents'=> '350',
-        'pro_gear_weight'=> '0',
-        'pro_gear_weight_spouse'=> '0',
-        'slug'=> 'service-academy-cadet-midshipman',
-        ],
-      'aviation-cadet'=> [
-        'rank'=> 'Aviation Cadet',
-        'total_weight_self'=> '7000',
-        'total_weight_self_plus_dependents'=> '8000',
-        'pro_gear_weight'=> '2000',
-        'pro_gear_weight_spouse'=> '500',
-        'slug'=> 'aviation-cadet',
-        ],
-      'e-1'=> [
-        'rank'=> 'E-1',
+      'rank-1'=> [
+        'rank'=> 'Rank 1',
         'total_weight_self'=> '5000',
         'total_weight_self_plus_dependents'=> '8000',
         'pro_gear_weight'=> '2000',
         'pro_gear_weight_spouse'=> '500',
-        'slug'=> 'e-1',
-        ],
-      'e-2'=> [
-        'rank'=> 'E-2',
-        'total_weight_self'=> '5000',
-        'total_weight_self_plus_dependents'=> '8000',
-        'pro_gear_weight'=> '2000',
-        'pro_gear_weight_spouse'=> '500',
-        'slug'=> 'e-2',
-        ],
-      'e-3'=> [
-        'rank'=> 'E-3',
-        'total_weight_self'=> '5000',
-        'total_weight_self_plus_dependents'=> '8000',
-        'pro_gear_weight'=> '2000',
-        'pro_gear_weight_spouse'=> '500',
-        'slug'=> 'e-3',
-        ],
-      'e-4'=> [
-        'rank'=> 'E-4',
-        'total_weight_self'=> '7000',
-        'total_weight_self_plus_dependents'=> '8000',
-        'pro_gear_weight'=> '2000',
-        'pro_gear_weight_spouse'=> '500',
-        'slug'=> 'e-4',
-        ],
-      'e-5'=> [
-        'rank'=> 'E-5',
-        'total_weight_self'=> '7000',
-        'total_weight_self_plus_dependents'=> '9000',
-        'pro_gear_weight'=> '2000',
-        'pro_gear_weight_spouse'=> '500',
-        'slug'=> 'e-5',
-        ],
-      'e-6'=> [
-        'rank'=> 'E-6',
-        'total_weight_self'=> '8000',
-        'total_weight_self_plus_dependents'=> '11000',
-        'pro_gear_weight'=> '2000',
-        'pro_gear_weight_spouse'=> '500',
-        'slug'=> 'e-6',
-        ],
-      'e-7'=> [
-        'rank'=> 'E-7',
-        'total_weight_self'=> '11000',
-        'total_weight_self_plus_dependents'=> '13000',
-        'pro_gear_weight'=> '2000',
-        'pro_gear_weight_spouse'=> '500',
-        'slug'=> 'e-7',
-        ],
-      'e-8'=> [
-        'rank'=> 'E-8',
-        'total_weight_self'=> '12000',
-        'total_weight_self_plus_dependents'=> '14000',
-        'pro_gear_weight'=> '2000',
-        'pro_gear_weight_spouse'=> '500',
-        'slug'=> 'e-8',
-        ],
-      'e-9'=> [
-        'rank'=> 'E-9',
-        'total_weight_self'=> '13000',
-        'total_weight_self_plus_dependents'=> '15000',
-        'pro_gear_weight'=> '2000',
-        'pro_gear_weight_spouse'=> '500',
-        'slug'=> 'e-9',
-        ],
-      'o-1-w-1-service-academy-graduate'=> [
-        'rank'=> 'O-1/W-1/Service Academy Graduate',
-        'total_weight_self'=> '10000',
-        'total_weight_self_plus_dependents'=> '12000',
-        'pro_gear_weight'=> '2000',
-        'pro_gear_weight_spouse'=> '500',
-        'slug'=> 'o-1-w-1-service-academy-graduate',
-        ],
-      'o-2-w-2'=> [
-        'rank'=> 'O-2/W-2',
-        'total_weight_self'=> '12500',
-        'total_weight_self_plus_dependents'=> '13500',
-        'pro_gear_weight'=> '2000',
-        'pro_gear_weight_spouse'=> '500',
-        'slug'=> 'o-2-w-2',
-        ],
-      'o-3-w-3'=> [
-        'rank'=> 'O-3/W-3',
+        'slug'=> 'rank-1',
+      ],
+      'rank-2'=> [
+        'rank'=> 'Rank 2',
         'total_weight_self'=> '13000',
         'total_weight_self_plus_dependents'=> '14500',
         'pro_gear_weight'=> '2000',
         'pro_gear_weight_spouse'=> '500',
-        'slug'=> 'o-3-w-3',
-        ],
-      'o-4-w-4'=> [
-        'rank'=> 'O-4/W-4',
-        'total_weight_self'=> '14000',
-        'total_weight_self_plus_dependents'=> '17000',
-        'pro_gear_weight'=> '2000',
-        'pro_gear_weight_spouse'=> '500',
-        'slug'=> 'o-4-w-4',
-        ],
-      'o-5-w-5'=> [
-        'rank'=> 'O-5/W-5',
-        'total_weight_self'=> '16000',
-        'total_weight_self_plus_dependents'=> '17500',
-        'pro_gear_weight'=> '2000',
-        'pro_gear_weight_spouse'=> '500',
-        'slug'=> 'o-5-w-5',
-        ],
-      'o-6'=> [
-        'rank'=> 'O-6',
+        'slug'=> 'rank-2',
+      ],
+      'rank-3'=> [
+        'rank'=> 'Rank 3',
         'total_weight_self'=> '18000',
         'total_weight_self_plus_dependents'=> '18000',
         'pro_gear_weight'=> '2000',
         'pro_gear_weight_spouse'=> '500',
-        'slug'=> 'o-6',
-        ],
-      'o-7'=> [
-        'rank'=> 'O-7',
-        'total_weight_self'=> '18000',
-        'total_weight_self_plus_dependents'=> '18000',
-        'pro_gear_weight'=> '2000',
-        'pro_gear_weight_spouse'=> '500',
-        'slug'=> 'o-7',
-        ],
-      'o-8'=> [
-        'rank'=> 'O-8',
-        'total_weight_self'=> '18000',
-        'total_weight_self_plus_dependents'=> '18000',
-        'pro_gear_weight'=> '2000',
-        'pro_gear_weight_spouse'=> '500',
-        'slug'=> 'o-8',
-        ],
-      'o-9'=> [
-        'rank'=> 'O-9',
-        'total_weight_self'=> '18000',
-        'total_weight_self_plus_dependents'=> '18000',
-        'pro_gear_weight'=> '2000',
-        'pro_gear_weight_spouse'=> '500',
-        'slug'=> 'o-9',
-        ],
-      'o-10'=> [
-        'rank'=> 'O-10',
-        'total_weight_self'=> '18000',
-        'total_weight_self_plus_dependents'=> '18000',
-        'pro_gear_weight'=> '2000',
-        'pro_gear_weight_spouse'=> '500',
-        'slug'=> 'o-10',
-        ],
-      'civilian-employee'=> [
-        'rank'=> 'Civilian Employee',
-        'total_weight_self'=> '18000',
-        'total_weight_self_plus_dependents'=> '18000',
-        'pro_gear_weight'=> '2000',
-        'pro_gear_weight_spouse'=> '500',
-        'slug'=> 'civilian-employee',
-        ],
-      ];
+        'slug'=> 'rank-3',
+      ],
+    ];
+  }
+
+  private function zip3s() {
+    return [
+      [
+        'zip3'=> '220',
+        'basepoint_city'=> '',
+        'state'=> '',
+        'service_area'=> '168',
+        'rate_area'=> '25',
+        'region'=> '10',
+      ],
+      [
+        'zip3'=> '902',
+        'basepoint_city'=> '',
+        'state'=> '',
+        'service_area'=> '56',
+        'rate_area'=> '88',
+        'region'=> '2',
+      ],
+    ];
+  }
+
+  private function zipCodes() {
+    return [
+      [
+        'code' => '22030',
+        'city' => 'Fairfax',
+        'state' => 'VA',
+        'lat' => '38.853231',
+        'lon' => '-77.305097',
+      ],
+      [
+        'code' => '90210',
+        'city' => 'Beverly Hills',
+        'state' => 'CA',
+        'lat' => '33.7865940000',
+        'lon' => '-118.2986620000',
+      ],
+    ];
+  }
+  
+  private function serviceAreas() {
+    return [
+      [
+        'service_area' => '168',
+        'name' => '',
+        'services_schedule' => '3',
+        'linehaul_factor' => '2.18',
+        'orig_dest_service_charge' => '6.34',
+        'year' => '2018',
+      ],
+      [
+        'service_area' => '56',
+        'name' => '',
+        'services_schedule' => '3',
+        'linehaul_factor' => '2.68',
+        'orig_dest_service_charge' => '7.75',
+        'year' => '2018',
+      ],
+    ];
   }
 
 }
