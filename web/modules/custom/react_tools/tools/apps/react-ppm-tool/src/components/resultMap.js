0f22a563f6aebfeaef142a9ca2f47719ff6306fc
Justin <justin.sarnik@bixal.com>
2018-10-04 11:54:14 -0400

    refactor maps to use google maps API, implement env variables in build tools

--- web/modules/custom/react_tools/tools/apps/react-ppm-tool/src/components/resultMap.js
+++ web/modules/custom/react_tools/tools/apps/react-ppm-tool/src/components/resultMap.js
@@ -1,46 +1,86 @@
-
 import React, { Component }  from 'react';
-import { Map, TileLayer, Marker, Polyline } from 'react-leaflet'
 import * as _ from 'lodash';
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker,
+  Polyline
+} from "react-google-maps";
+const { compose, lifecycle } = require("recompose");
+const apiKey = process.env.GOOGLE_MAPS_API_KEY;
+const gMapUrl = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&v=3.exp&libraries=geometry,drawing,places`;
 
 class ResultMap extends Component {
 
     markerComponent = () =>{
       return (
         _.map(this.props.map.coords, (coord, i)=>{
-          return (
-            <Marker position={coord} key={i}>
-            </Marker>
-          )
+          return <Marker key={i} position={this.getGoogleMapsCoordObject(coord)} />
         })
       )
     }
 
-    render() {
-       let polylinePos = [this.props.map.coords.origin, this.props.map.coords.destination];
-       let bounds = [
-         [
-          this.props.map.coords.destination[0],
-          this.props.map.coords.destination[1]
-         ],
-         [
-          this.props.map.coords.origin[0],
-          this.props.map.coords.origin[1]
-         ]
-        ]
-       let lineColor = 'blue';
-       let zoom = 13;
-        return (
-          <Map center={this.props.map.center} zoom={zoom} id="map-container" bounds={bounds}>
-            <TileLayer
-              attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            {this.markerComponent()}
-            <Polyline positions={polylinePos} color={lineColor}/>
-          </Map>
-        )
+    getGoogleMapsCoordObject = (coord) =>{
+      return {
+        lat: parseFloat(coord[0]),
+        lng: parseFloat(coord[1]),
       }
+    }
+
+    render() {
+      let defaultMapOptions = {
+        disableDefaultUI: true,
+        zoomControl: true
+      }
+      let zoom = 13;
+      let lineColor = '#0400ff';
+      let polyLinePath = [
+        this.getGoogleMapsCoordObject(this.props.map.coords.origin),
+        this.getGoogleMapsCoordObject(this.props.map.coords.destination)
+      ];
+      let _bounds = {
+        origin: this.getGoogleMapsCoordObject(this.props.map.coords.origin),
+        dest: this.getGoogleMapsCoordObject(this.props.map.coords.destination)
+      };
+
+      const MapWithMarkers = compose(
+        lifecycle({
+          componentDidMount() {
+              this.setState({
+                  zoomToMarkers: map => {
+                      const bounds = new window.google.maps.LatLngBounds();
+                      _.each(map.props.outerBounds, (_b)=>{
+                        bounds.extend(new window.google.maps.LatLng(_b.lat, _b.lng));
+                      });
+                      map.fitBounds(bounds);
+                  }
+              })
+          },
+      }),
+      withScriptjs,
+      withGoogleMap
+      )(props =>
+        <GoogleMap 
+          outerBounds={_bounds}
+          ref={props.zoomToMarkers}
+          defaultZoom={zoom}
+          defaultOptions={defaultMapOptions}
+          defaultCenter={this.getGoogleMapsCoordObject(this.props.map.center)}>
+          {this.markerComponent()}
+          <Polyline path={polyLinePath} options={{strokeColor: lineColor, strokeWeight: '3', strokeOpacity: '1'}}/>
+        </GoogleMap>
+      );
+      
+      return (
+        <MapWithMarkers
+          googleMapURL={gMapUrl}
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: `300px` }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+      />
+      )
+    }
 }
 
 export default ResultMap;
