88f6249a8a8e34dea579e9bd33fcf634f2667afd
Tyler Thompson <tyler.thompson@bixal.com>
2019-04-22 19:36:21 -0400

    adjusted indenting to be more readable

--- .ebextensions/post-deploy.sh
+++ .ebextensions/post-deploy.sh
@@ -7,13 +7,13 @@ commands:
         ignoreErrors: true
 files:
     "/opt/elasticbeanstalk/hooks/appdeploy/post/99-drupal-updates.sh":
-    mode: "000755"
-    owner: root
-    group: root
-    contents: |
-        #!/usr/bin/env bash
-        container_id=$(sudo docker ps -q)
-        sudo docker exec -it ${container_id} vendor/bin/drush updatedb -y
-        sudo docker exec -it ${container_id} vendor/bin/drush cim -y
-        sudo docker exec -it ${container_id} vendor/bin/drush cim -y --partial --source=modules/custom/custom_move_mil_menus/config/install/
-        sudo docker exec -it ${container_id} vendor/bin/drush cr -y
+        mode: "000755"
+        owner: root
+        group: root
+        contents: |
+            #!/usr/bin/env bash
+            container_id=$(sudo docker ps -q)
+            sudo docker exec -it ${container_id} vendor/bin/drush updatedb -y
+            sudo docker exec -it ${container_id} vendor/bin/drush cim -y
+            sudo docker exec -it ${container_id} vendor/bin/drush cim -y --partial --source=modules/custom/custom_move_mil_menus/config/install/
+            sudo docker exec -it ${container_id} vendor/bin/drush cr -y
