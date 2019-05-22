bb8a158c2276bb5ee21f1282a8cae4524268d453
Tyler Thompson <tyler.thompson@bixal.com>
2019-05-22 17:05:42 -0400

    convert the automatic hook script to a manual script

---
+++ post-deploy.sh
@@ -0,0 +1,12 @@
+# script to run after deployment of new image
+# script contains commands that always should be run immediately after manually deploying
+
+#!/usr/bin/env bash
+container_id=$(docker ps -q)
+docker exec -i ${container_id} vendor/bin/drush sset system.maintenance_mode 1
+docker exec -i ${container_id} vendor/bin/drush updatedb -y
+docker exec -i ${container_id} vendor/bin/drush entity-updates -y
+docker exec -i ${container_id} vendor/bin/drush cim -y
+docker exec -i ${container_id} vendor/bin/drush entity-updates -y
+docker exec -i ${container_id} vendor/bin/drush cr -y
+docker exec -i ${container_id} vendor/bin/drush sset system.maintenance_mode 0
