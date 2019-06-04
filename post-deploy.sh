de732d1935eed7f965acae86f63263a8fb339919
Tyler Thompson <tyler.thompson@bixal.com>
2019-06-04 11:17:09 -0400

    updated config import command to use drupal not drush

--- post-deploy.sh
+++ post-deploy.sh
@@ -6,7 +6,7 @@ container_id=$(docker ps -q)
 docker exec -i ${container_id} vendor/bin/drush sset system.maintenance_mode 1
 docker exec -i ${container_id} vendor/bin/drush updatedb -y
 docker exec -i ${container_id} vendor/bin/drush entity-updates -y
-docker exec -i ${container_id} vendor/bin/drush cim -y
+docker exec -i ${container_id} php drupal config:import -y
 docker exec -i ${container_id} vendor/bin/drush entity-updates -y
 docker exec -i ${container_id} vendor/bin/drush cr -y
 docker exec -i ${container_id} vendor/bin/drush sset system.maintenance_mode 0
