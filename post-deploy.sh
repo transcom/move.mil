f8d7c7acdaf346a6d7f77b643de039fbfb04e68b
Tyler Thompson <tyler.thompson@bixal.com>
2019-06-04 12:11:11 -0400

    updated script to be run inside docker

--- post-deploy.sh
+++ post-deploy.sh
@@ -1,12 +1,18 @@
+#!/bin/bash
 # script to run after deployment of new image
 # script contains commands that always should be run immediately after manually deploying
 
-#!/usr/bin/env bash
-container_id=$(docker ps -q)
-docker exec -i ${container_id} vendor/bin/drush sset system.maintenance_mode 1
-docker exec -i ${container_id} vendor/bin/drush updatedb -y
-docker exec -i ${container_id} vendor/bin/drush entity-updates -y
-docker exec -i ${container_id} php drupal config:import -y
-docker exec -i ${container_id} vendor/bin/drush entity-updates -y
-docker exec -i ${container_id} vendor/bin/drush cr -y
-docker exec -i ${container_id} vendor/bin/drush sset system.maintenance_mode 0
+echo 'Setting site in maintenance mode...'
+vendor/bin/drush sset system.maintenance_mode 1
+echo 'Updating database...'
+vendor/bin/drush updatedb -y
+echo 'Updating entities...'
+vendor/bin/drush entity-updates -y
+echo 'Importing configuration changes...'
+php drupal config:import -y
+echo 'Updating entities...'
+vendor/bin/drush entity-updates -y
+echo 'Rebuilding cache...'
+vendor/bin/drush cr -y
+echo 'Exiting maintenance mode...'
+vendor/bin/drush sset system.maintenance_mode 0
