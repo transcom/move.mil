9b5e33e26551127aba0b9491d73148ab7de60f17
Justin <justin.sarnik@bixal.com>
2020-01-28 12:35:08 -0500

    return true for npm run test as we have no tests atm

--- web/modules/custom/react_tools/tools/scripts/test.js
+++ web/modules/custom/react_tools/tools/scripts/test.js
@@ -5,6 +5,8 @@ process.env.BABEL_ENV = 'test';
 process.env.NODE_ENV = 'test';
 process.env.PUBLIC_URL = '';
 
+console.log('No tests are currently implemented.')
+return true;
 // Makes the script crash on unhandled rejections instead of silently
 // ignoring them. In the future, promise rejections that are not handled will
 // terminate the Node.js process with a non-zero exit code.
