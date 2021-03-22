91df9c6966491db4529a488b25d653f5fd05e792
Carter Jones <carter@truss.works>
2021-03-22 15:28:10 -0700

    use the new name of the webpack manifest plugin constructor

--- web/modules/custom/react_tools/tools/config/webpack.config.js
+++ web/modules/custom/react_tools/tools/config/webpack.config.js
@@ -14,7 +14,7 @@ const TerserPlugin = require('terser-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const safePostCssParser = require('postcss-safe-parser');
-const ManifestPlugin = require('webpack-manifest-plugin');
+const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
 const InterpolateHtmlPlugin = require('react-dev-utils/InterpolateHtmlPlugin');
 const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
 const WatchMissingNodeModulesPlugin = require('react-dev-utils/WatchMissingNodeModulesPlugin');
@@ -541,7 +541,7 @@ function getConfig(webpackEnv, appName) {
               // Generate a manifest file which contains a mapping of all asset filenames
               // to their corresponding output file so that tools can pick it up without
               // having to parse `index.html`.
-              new ManifestPlugin({
+              new WebpackManifestPlugin({
                 fileName: 'asset-manifest.json',
                 publicPath: publicPath,
                 generate: (seed, files) => {
